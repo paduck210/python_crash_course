dea29950ff94e075ec4265efa75de710e524f8b8
Jung Yu <hjyu1408@gmail.com>
2019-12-25 20:50:21 -0500

    "log practce"

---
+++ day11_codewars/practice.py
@@ -0,0 +1,22 @@
+import math
+
+def isPP(x):
+    for i in range(2,x):
+        if i ** int(round(math.log(x, i))) == x:
+            y = int(round(math.log(x, i)))
+            # 3 = int(round(math.log(8, 2)))
+            # 2 = int(round(math.log(36, 6)))
+            return([i, y])
+
+
+print(isPP(17))
+print(isPP(343))
+print(isPP(36))
+print(isPP(81))
+
+
+
+# isPP(4) => [2,2]
+# isPP(8) => [2,3]
+# isPP(9) => [3,2]
+# isPP(5) => None
