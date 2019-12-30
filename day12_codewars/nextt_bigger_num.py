5e3d5170a21cdf88e23a55f4ad3f48bc62f3c4ae
Jung Yu <hjyu1408@gmail.com>
2019-12-29 22:18:55 -0500

    "keep trying"

--- day12_codewars/nextt_bigger_num.py
+++ day12_codewars/nextt_bigger_num.py
@@ -1,18 +1,10 @@
 def next_bigger(n):
-    # your code here
+# your code here
 
-    l = [int(i) for i in str(n)]  # [1, 3, 5] #l[-(len(l), -(len(l+1), -(len(l+2)]
-    for i in l:
-        current_index = list.index(i)
-        next_index = current_index + 1
-        if current_index > 0:
-
+    list = [int(i) for i in str(n)]
+    list.reverse()
+    for n in list:
+        print(n)
 
 
-
-
-    # l = l.reverse()  # [5, 3, 1]
-    for i in l:
-        if l[current_index] ==
-        elif l[current_index] == -2
-        elif l[current_index] != l[-2] and l[current_index] > l[next_index]  # 5 > 3
+next_bigger(123)
