89821d696a988408fc8229e547be1f42c53e83a8
Jung Yu <hjyu1408@gmail.com>
2019-12-30 00:10:06 -0500

    "practice append and list of python"

--- day13_codewars/numbers.py
+++ day13_codewars/numbers.py
@@ -4,14 +4,25 @@ def solution(args):
     add_list = []
     for i in args:
         if i-1 in args:
-            add_list.append([i])
+            n = add_list.pop() + [i]
+            add_list.append(n)
         else:
-            add_list[-1:] += [i]
+            add_list.append([i])
 
-    add_list.fl
     print(add_list)
 
+    string = ""
+    for l in add_list:
+        if len(l) == 1:
+            string += f"{l[0]},"
+        elif len(l) == 2:
+            string += f"{l[0]},{l[-1]},"
+        else:
+            string += f"{l[0]}-{l[-1]},"
+    return(string[0:-1])
 
 
-solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20])
+
+print(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]))
+# returns "-6,-3-1,3-5,7-11,14,15,17-20"
 
